eea5f25c0c500ec84b3f05d38177ce115f095846
muurastr <muura@mail.ru>
2016-02-18 16:05:31 +0300

    localStorage variable x

--- public/app.js
+++ public/app.js
@@ -1,7 +1,12 @@
 function something()
 	{
-		var x = window.localStorage.getItem('aaa');
-		window.localStorage.setItem('aaa', 555);
+		// получаем значение из localStorage
+		var x = window.localStorage.getItem('b');
+		// уеличиваем это значение на 1 (x - переменная типа string)
+		// для того, чтобы сделать ее int, надо умножить ее на 1
+		x = x * 1 + 1; 
+		// устанавливаем значение ключа b равным x
+		window.localStorage.setItem('b', x);
 		alert(x);
 		
 	}
