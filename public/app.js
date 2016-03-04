911c47ab8f13ac12f2d71ffdaa603dfe6627a992
muurastr <muura@mail.ru>
2016-03-04 16:33:36 +0300

    Display products step 1

--- public/app.js
+++ public/app.js
@@ -1,12 +1,14 @@
 function something()
 	{
 		// получаем значение из localStorage
-		var x = window.localStorage.getItem('b');
+		var x = window.localStorage.getItem('b'); // x = hh['b']
 		// уеличиваем это значение на 1 (x - переменная типа string)
 		// для того, чтобы сделать ее int, надо умножить ее на 1
-		x = x * 1 + 1; 
+		// возвращает объект типа string, поэтому мы используем умножение
+		// как самый простой способ преобразование строки в int
+		x = x * 1 + 1; // x = x + 1
 		// устанавливаем значение ключа b равным x
-		window.localStorage.setItem('b', x);
+		window.localStorage.setItem('b', x); // hh['b'] = x
 		alert(x);
 		
 	}
