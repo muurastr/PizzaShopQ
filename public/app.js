d3fb20718233a4ca36d50041fe1596fb178b854c
muurastr <muura@mail.ru>
2016-03-14 23:13:53 +0300

    Add cart_get_number_of_items function

--- public/app.js
+++ public/app.js
@@ -15,10 +15,35 @@ function something()
 
 function add_to_cart(id)
 	{
-		var key = 'product_' + id;
+		var key = 'product_' + id; 
+		// выделение переменной = extract variable
 		var x = window.localStorage.getItem(key);
 		// т.е. id хеша будет выглядеть "product_1" 
 		// чтобы можно было различать объекты, которые хранятся в localStorage
 		x = x * 1 + 1;
 		window.localStorage.setItem(key, x);
+
+// вывод кол-ва пицц в корзине
+		alert('Items in your cart: ' + cart_get_number_of_items());
 	}
+
+function cart_get_number_of_items()
+{
+	var cnt = 0;
+
+	for(var i = 0; i < window.localStorage.length; i++)
+	{
+		var key = window.localStorage.key(i); //получаем ключ
+		var value = window.localStorage.getItem(key); //получаем значение
+		// аналог в ruby:
+		// hh[key] = x
+
+		// теперь надо найти ключи, начинающиеся c "product_"
+		if(key.indexOf('product_') == 0);
+		{
+			cnt = cnt + value * 1;
+		}
+	}
+
+	return cnt;
+}
